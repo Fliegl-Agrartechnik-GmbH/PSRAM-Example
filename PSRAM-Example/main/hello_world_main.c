49f5d64c51a29ad901b651865bcc9b1bbcbb0515
Franz Höpfinger <franz.hoepfinger@fliegl.com>
2020-10-13 17:18:08 +0200

    Read Speed 32bit ArraySize 2048kb  time: 1.4 14.3 mb/sec

--- PSRAM-Example/main/hello_world_main.c
+++ PSRAM-Example/main/hello_world_main.c
@@ -26,44 +26,6 @@ int RamTest()
 	char xx = 0;
 	for (int a = 0; a < 13; a++)
 		{
-		printf("Read Speed 8bit ArraySize %4dkb ", rs[a]);
-		int ramsize = rs[a] * 1024;
-		char * rm = (char*)malloc(ramsize);
-
-		int iters = 10; // Just enuff to boot the dog
-		if (rs[a] < 512) iters = 50;
-		double st = GetTime();
-		for (int b = 0; b < iters; b++)
-			for (int c = 0; c < ramsize; c++)
-				xx |= rm[c];
-		st = GetTime() - st;
-		vTaskDelay(1); // Dog it!
-		double speed = ((double)(iters*ramsize ) / (1024 * 1024)) / (st);
-		printf(" time: %2.1f %2.1f mb/sec  \n", st, speed);
-		free(rm);
-		}
-	printf("\n");
-	for (int a = 0; a < 13; a++)
-		{
-		printf("Read Speed 16bit ArraySize %4dkb ", rs[a]);
-		int ramsize = rs[a] * 1024;
-		short * rm = (short*)malloc(ramsize);
-
-		int iters = 10; // Just enuff to boot the dog
-		if (rs[a] < 512) iters = 50;
-		double st = GetTime();
-		for (int b = 0; b < iters; b++)
-			for (int c = 0; c < ramsize/2; c++)
-				xx |= rm[c];
-		st = GetTime() - st;
-		vTaskDelay(1); // Dog it!
-		double speed = ((double)(iters*ramsize) / (1024 * 1024)) / (st);
-		printf(" time: %2.1f %2.1f mb/sec  \n", st, speed);
-		free(rm);
-		}
-	printf("\n");
-	for (int a = 0; a < 13; a++)
-		{
 		printf("Read Speed 32bit ArraySize %4dkb ", rs[a]);
 		int ramsize = rs[a] * 1024;
 		int * rm = (int*)malloc(ramsize);
